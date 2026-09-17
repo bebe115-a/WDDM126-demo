d61906f7f207336f9c39509c12823a13d06791d7
bebe115-a <rhp8swnrwz@privaterelay.appleid.com>
2026-09-17 09:52:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// WDDM 126 Demo Script
+
+// This file is intentionally simple for Week 1
+
+
+
+console.log("WDDM 126 demo project loaded successfully");
+
+
+
+// Example function
+
+function showWelcomeMessage() {
+
+ alert("Welcome to WDDM 126 – Tools & Workflow!");
+
+}
+
+
+
+// Run once when page loads
+
+showWelcomeMessage();
 
